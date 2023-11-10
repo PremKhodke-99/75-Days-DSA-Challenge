7d51751b8a4534c8c21a59d33ed00ac881b414f4
Prem <premkhodke.99@gmail.com>
2023-11-10 23:10:54 +0530

    Day 15

--- Day 15/ZeroOnes.js
+++ Day 15/ZeroOnes.js
@@ -1,6 +1,24 @@
 var zeroOnes = (nums) => {
-    let ind = nums.indexOf(0);
-    console.log(ind);
+    // let ind = nums.indexOf(0);
+    // console.log(ind);
+
+    let i = 0, j = nums.length - 1, res = -1;
+
+    let target = 1;
+
+    while(i <= j){
+        let mid = Math.floor((i+j)/2);
+        if(nums[mid] == target){
+            res = mid;
+            j = mid  - 1;
+        }else if(nums[mid] < target){
+            i = mid + 1;
+        }else{
+            j = mid - 1;
+        }
+    }
+
+    console.log(res);
 }
 
 let nums = [0, 0, 1, 1];

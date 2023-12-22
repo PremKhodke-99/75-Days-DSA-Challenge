ccb791fd859dd8036a40a63b15ea139915267182
Prem <premkhodke.99@gmail.com>
2023-12-22 12:22:52 +0530

    Day 07

--- Day 07/ModOfNumber.js
+++ Day 07/ModOfNumber.js
@@ -1,15 +1,15 @@
-let modulusOfNumberWithoutRecursion = (a , b) => {
+let modulusOfNumberWithRecursion = (a , b) => {
     if(b === 0) 
         return -1;
 
     if(a < b) 
         return a;
 
-    return modulusOfNumberWithoutRecursion(a-b,b);
+    return modulusOfNumberWithRecursion(a-b,b);
 }
 
 function main(){
-    let res = modulusOfNumberWithoutRecursion(10, 3);
+    let res = modulusOfNumberWithRecursion(10, 3);
     console.log(res);
 }
 
